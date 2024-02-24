143dc0907019c5ce7135c338e26d3996f4f84c21
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2024-02-24 10:30:31 +0530

    While loop

---
+++ Feb24_Loops/src/test/SampleA.java
@@ -0,0 +1,22 @@
+package test;
+
+public class SampleA {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Start execution");
+		
+		int a=1;
+		while(a <= 5)
+		{
+		System.out.println("ABCABC");
+		a=a+1;
+		}
+		
+		
+
+		System.out.println("End execution");
+	}
+
+}
